642a843d88f2b6066bca4ce40b2da6fdb8bb1f53
jgustavoj <gjime011@fiu.edu>
2020-09-05 01:07:59 +0000

    Initial Commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,47 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+function Counter(props) {
+	let mystyle = {
+		color: "white",
+		backgroundColor: "#3e4348",
+		padding: "10px",
+		fontFamily: "Arial",
+		fontSize: "25px",
+		borderRadius: "8px",
+		height: "100px"
+	};
+
+	return (
+		<div className="container">
+			<div
+				className="row text-center align-items-center  "
+				style={{ background: "black" }}>
+				<div className="col ml-3 mr-3" style={mystyle}>
+					<i className="far fa-clock fa-3x" />
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+				<div className="col mr-3" style={mystyle}>
+					0
+				</div>
+			</div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
